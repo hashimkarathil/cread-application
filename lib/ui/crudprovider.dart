0d9011a9d9d6e07f084443399974083d22df74a8
hashimkarathil <karathilhashim@gmail.com>
2025-10-20 14:30:40 +0530

    Latest

--- lib/ui/crudprovider.dart
+++ lib/ui/crudprovider.dart
@@ -1,5 +1,21 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:tudoapp/ui/usermodel.dart';
 
 class Crudprovider extends ChangeNotifier{
-
+  List <Usermodel> users=[];
+  //adding data using model
+  Future <void> addData(Usermodel usermodel) async {
+    final usercollection = FirebaseFirestore.instance.collection("User");
+    final id=usercollection.doc().id;
+    final user= usermodel.Copywith(id: id);
+    await
+    usercollection.doc(id).set(user.toMap());
+  }
+  Future <void> fetchdata() async {
+    final userCollection = await FirebaseFirestore.instance.collection("Users").get();
+    final user1 = userCollection.docs.map((doc) => Usermodel.fromMap(doc.data()));
+    users.addAll(user1);
+  }
 }
+
--- lib/ui/todologinscreen.dart
+++ lib/ui/todologinscreen.dart
@@ -1,6 +1,9 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 //import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:tudoapp/ui/crudprovider.dart';
+import 'package:tudoapp/ui/usermodel.dart';
 
 class Todologinscreen extends StatefulWidget {
   const Todologinscreen({super.key});
@@ -12,6 +15,7 @@ class Todologinscreen extends StatefulWidget {
 class _TodologinscreenState extends State<Todologinscreen> {
   TextEditingController emailController=TextEditingController();
   TextEditingController passwordController=TextEditingController();
+  TextEditingController phonenumberController=TextEditingController();
   
   @override
   Widget build(BuildContext context) {
@@ -19,121 +23,153 @@ class _TodologinscreenState extends State<Todologinscreen> {
           appBar: AppBar(),
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 20,vertical: 70),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-         
-          children: [
+        child: Consumer <Crudprovider>(
+          builder: (BuildContext context,  State, Widget? child) { return 
+          Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
            
-            Column(
-               crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Center(
-                  child: Text(
-                    "Authentication",
-                    style: TextStyle(
-                      fontWeight: FontWeight.w700,
-                      fontSize: 25,
-                      color: Colors.black,
+            children: [
+             
+              Column(
+                 crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Center(
+                    child: Text(
+                      "Authentication",
+                      style: TextStyle(
+                        fontWeight: FontWeight.w700,
+                        fontSize: 25,
+                        color: Colors.black,
+                      ),
                     ),
                   ),
+               
+               SizedBox(height: 50),
+              Text(
+                'Please Enter Your Email Id:',
+                style: TextStyle(
+                  fontSize: 15,
+                  color: Colors.black,
                 ),
-             
-             SizedBox(height: 50),
-            Text(
-              'Please Enter Your Email Id:',
-              style: TextStyle(
-                fontSize: 15,
-                color: Colors.black,
               ),
-            ),
-            SizedBox(height: 10,),
-            TextFormField(
-              controller: emailController,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(3),
-
-                  borderSide: BorderSide(color: Colors.red, width: 1),
+              SizedBox(height: 10,),
+              TextFormField(
+                controller: emailController,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(3),
+          
+                    borderSide: BorderSide(color: Colors.red, width: 1),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.red, width: 1),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.red, width: 1),
+                  ),
+                  hintText: 'Email',
+                  hintStyle: TextStyle(color: Colors.grey),
                 ),
-                enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.red, width: 1),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.red, width: 1),
-                ),
-                hintText: 'Email',
-                hintStyle: TextStyle(color: Colors.grey),
               ),
-            ),
-            SizedBox(height: 10),
-
-            Text(
-              'Please Enter Your Password:',
-              style: TextStyle(
-                fontSize: 15,
-                color: Colors.black,
+              SizedBox(height: 10),
+          
+              Text(
+                'Please Enter Your Password:',
+                style: TextStyle(
+                  fontSize: 15,
+                  color: Colors.black,
+                ),
               ),
-            ),
-            SizedBox(height: 10,),
-            TextFormField(
-              controller: passwordController,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(3),
-
-                  borderSide: BorderSide(color: Colors.red, width: 1),
+              SizedBox(height: 10,),
+              TextFormField(
+                controller: passwordController,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(3),
+          
+                    borderSide: BorderSide(color: Colors.red, width: 1),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.red, width: 1),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.red, width: 1),
+                  ),
+                  hintText: 'Password',
+                  hintStyle: TextStyle(color: Colors.grey),
                 ),
-                enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.red, width: 1),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.red, width: 1),
-                ),
-                hintText: 'Password',
-                hintStyle: TextStyle(color: Colors.grey),
               ),
-            ),
-            SizedBox(height: 20,),
-
-             ],
-            ),
-            Column(
-              children: [
-                 InkWell(
-                    onTap: () {
-                     FirebaseFirestore.instance.collection("Users").add({"email":emailController.text,"password":passwordController.text});
-                     
-                    },
-                    child: Center(
-                      child: Container(
-                        width: 150,
-                        height: 50,
-                        decoration: BoxDecoration(
-                          color: Colors.red,
-                          borderRadius: BorderRadius.circular(3),
-                        ),
-
-                        child: Center(
-                          child: Text(
-                            "Submit",
-                            style: TextStyle(color: Colors.white, fontSize: 20),
-                            textAlign: TextAlign.center,
+              SizedBox(height: 10,),
+              Text(
+                'Please Enter Your Phone Number:',
+                style: TextStyle(
+                  fontSize: 15,
+                  color: Colors.black,
+                ),
+              ),
+              SizedBox(height: 10,),
+              TextFormField(
+                controller: phonenumberController,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(3),
+          
+                    borderSide: BorderSide(color: Colors.red, width: 1),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.red, width: 1),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.red, width: 1),
+                  ),
+                  hintText: 'Phone Number',
+                  hintStyle: TextStyle(color: Colors.grey),
+                ),
+              ),
+               ],
+              ),
+              Column(
+                children: [
+                   InkWell(
+                      onTap: () {
+                       //FirebaseFirestore.instance.collection("Users").add({"email":emailController.text,"password":passwordController.text,"phonenumber":phonenumberController.text});
+                       State.addData(Usermodel(
+                        email: emailController.text,password: passwordController.text,phonenumber: phonenumberController.text,
+                       ));
+                      },
+                      child: Center(
+                        child: Container(
+                          width: 150,
+                          height: 50,
+                          decoration: BoxDecoration(
+                            color: Colors.red,
+                            borderRadius: BorderRadius.circular(3),
+                          ),
+          
+                          child: Center(
+                            child: Text(
+                              "Submit",
+                              style: TextStyle(color: Colors.white, fontSize: 20),
+                              textAlign: TextAlign.center,
+                            ),
                           ),
                         ),
                       ),
                     ),
-                  ),
-
-                  ElevatedButton(
-                    onPressed: () async {
-                    final ret = await FirebaseFirestore.instance.collection("Users").get();
-                    for( var doc in ret.docs){
-                      print(doc.data());
-                    }
-                  }, child: Text("Get")),
-              ],
-            )
-          ],
+          
+                    ElevatedButton(
+                      onPressed: () async {
+                      final ret = await FirebaseFirestore.instance.collection("Users").get();
+                      for( var doc in ret.docs){
+                        print(doc.data());
+                      }
+                    }, child: Text("Get")),
+                ],
+              )
+            ],
+          );
+          },
+           
         ),
       ),
       
